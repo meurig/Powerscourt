c21d39905039c19db2e424ccc07fd76dcc499619
Meurig Freeman <me@meurig.com>
2011-07-26 00:09:52 +0100

    Created GroupOfPeopleAdmin with filter horizontal

--- wggateway/admin.py
+++ wggateway/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from powerscourt.wggateway.models import Address, Client, Person, GroupOfPeople, SippProvider, SippAdminPerson, Sipp, OldAddress
 from powerscourt.wggateway.models import Syndicate, ProductProvider, Product, PurchaseForm, Currency, Purchase, Rent
 
+class GroupOfPeopleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('people',)
+    list_display = ('code', 'address',)
+
 admin.site.register(Address)
 admin.site.register(Client)
 admin.site.register(Person)
-admin.site.register(GroupOfPeople)
+admin.site.register(GroupOfPeople, GroupOfPeopleAdmin)
 admin.site.register(SippProvider)
 admin.site.register(SippAdminPerson)
 admin.site.register(Sipp)
