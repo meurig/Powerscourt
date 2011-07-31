92a9d6202603f362f18a8ffaa69c8e8bb29cf485
Meurig Freeman <me@meurig.com>
2011-07-31 21:09:46 +0100

    Addres LLC and LLCAdmin to the admin site

--- wggateway/admin.py
+++ wggateway/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
-from powerscourt.wggateway.models import Address, Client, Person, GroupOfPeople, SippProvider, SippAdminPerson, Sipp, OldAddress
+from powerscourt.wggateway.models import Address, Client, Person, GroupOfPeople, LLC, SippProvider, SippAdminPerson, Sipp, OldAddress
 from powerscourt.wggateway.models import Syndicate, ProductProvider, Product, PurchaseForm, Currency, Purchase, Rent
 
 class GroupOfPeopleAdmin(admin.ModelAdmin):
     filter_horizontal = ('people',)
     list_display = ('code', 'address',)
 
+class LLCAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'address',)
+
 admin.site.register(Address)
 admin.site.register(Client)
-admin.site.register(Person)
+admin.site.register(Currency)
 admin.site.register(GroupOfPeople, GroupOfPeopleAdmin)
-admin.site.register(SippProvider)
-admin.site.register(SippAdminPerson)
-admin.site.register(Sipp)
+admin.site.register(LLC, LLCAdmin)
 admin.site.register(OldAddress)
-admin.site.register(Syndicate)
+admin.site.register(Person)
 admin.site.register(ProductProvider)
 admin.site.register(Product)
 admin.site.register(PurchaseForm)
-admin.site.register(Currency)
 admin.site.register(Purchase)
 admin.site.register(Rent)
+admin.site.register(SippProvider)
+admin.site.register(SippAdminPerson)
+admin.site.register(Sipp)
+admin.site.register(Syndicate)
