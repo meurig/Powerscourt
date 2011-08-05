3d99c6b6c73a688c3b2558e1567ef28381aae50a
Meurig Freeman <me@meurig.com>
2011-08-06 00:12:06 +0100

    search_form changed to use form class - broken!

--- wggateway/forms.py
+++ wggateway/forms.py
@@ -4,3 +4,9 @@ class ContactForm(forms.Form):
     subject = forms.CharField()
     message = forms.CharField()
     email = forms.EmailField(required=False)
+
+class ClientSearchForm(forms.Form):
+    client_code = forms.CharField(required=False)
+    name = forms.CharField(required=False)
+    post_code = forms.CharField(required=False)
+    email = forms.CharField(required=False)
--- wggateway/views.py
+++ wggateway/views.py
@@ -2,20 +2,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail
 from django.shortcuts import render_to_response
 from wggateway.models import Client
-from wggateway.forms import ContactForm
+from wggateway.forms import ContactForm, ClientSearchForm
 
 def search(request):
-    error = False
-    if 'q' in request.GET:
+    if request.method == 'POST':
+    #if 'q' in request.GET:
         q = request.GET['q']
-        if not q:
-            error = True
-        else:
-            clients = Client.objects.filter(code__icontains=q)
-            return render_to_response('search_results.html',
-                    {'clients': clients, 'query': q})
-    return render_to_response('search_form.html',
-            {'error': error})
+        #if not q:
+            #error = True
+        #else:
+            #clients = Client.objects.filter(code__icontains=q)
+            #return render_to_response('search_results.html',
+                    #{'clients': clients, 'query': q})
+    else:
+        form = ClientSearchForm()
+    return render_to_response('search_form.html', {'form': form})
 
 def contact(request):
     if request.method == 'POST':
