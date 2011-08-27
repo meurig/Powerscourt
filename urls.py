32fda44b07ae9351977376338fe9b0e54788d796
Meurig Freeman <me@meurig.com>
2011-08-27 12:11:48 +0100

    made a new client section complete with subsidenav

--- urls.py
+++ urls.py
@@ -21,11 +21,10 @@ urlpatterns = patterns('',
     url(r'^home/$', TemplateView.as_view(template_name="home.html"), name="home"),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^clients/(?P<page>[0-9]*)$', ListView.as_view(
-        #model=Client,
         queryset=Client.objects.all().select_subclasses(),
         template_name="client_list.html",
         paginate_by=25,
-        )),
+        ), name="clients"),
     url(r'^client/(?P<pk>\w+)/$', DetailView.as_view(
         #model=Client,
         queryset=Client.objects.all().select_subclasses(),
