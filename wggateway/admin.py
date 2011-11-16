8b82f5017379a03e7d41c419e7db0d9583e739cf
Meurig Freeman <me@meurig.com>
2011-11-16 20:57:19 +0000

    Minor admin tweaks, mainly filter_lists

--- wggateway/admin.py
+++ wggateway/admin.py
@@ -17,9 +17,33 @@ class ClientAdmin(admin.ModelAdmin):
 class LLCAdmin(admin.ModelAdmin):
     list_display = ('code', 'name',)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('provider',)
+
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = ('code', 'notional',)
     #list_display = ('code', 'client', 'product', 'notional', 'notional_currency',)
+    list_filter = ('product__provider', 'product')
+    #fields = (
+        #'client',
+        #'product',
+        #'code',
+        #'active',
+        #'password',
+        #'start_date',
+        #'form',
+        #'notional',
+        #'notional_currency',
+        #'duration',
+        #'signature_date',
+        #'syndicate',
+        #'date_funds_to_syndicate',
+        #'shares_no',
+        #'shares_value',
+        #'shares_currency',
+        #'shares_cert_date',
+        #'shares_cert_sent_date',
+        #'notes',)
 
 class SippAdminPersonAddressInLine(admin.StackedInline):
     model = SippAdminPersonAddress
@@ -33,6 +57,9 @@ class SippAdminPersonInLine(admin.TabularInline):
 class SippProviderAdmin(admin.ModelAdmin):
     inlines = [SippAdminPersonInLine,]
 
+class SippAdmin(admin.ModelAdmin):
+    list_filter = ('company',)
+
 admin.site.register(Address)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Currency)
@@ -41,11 +68,11 @@ admin.site.register(LLC, LLCAdmin)
 admin.site.register(OldAddress)
 admin.site.register(Person)
 admin.site.register(ProductProvider)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(PurchaseForm)
 admin.site.register(Purchase, PurchaseAdmin)
 admin.site.register(Rent)
 admin.site.register(SippProvider, SippProviderAdmin)
 admin.site.register(SippAdminPerson,SippAdminPersonAdmin)
-admin.site.register(Sipp)
+admin.site.register(Sipp,SippAdmin)
 admin.site.register(Syndicate)
